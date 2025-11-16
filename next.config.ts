7086c6496c6dfcc143bdd72b2f965039b0a2c642
Jirayuwat Nawachatsagul <>
2025-11-17 03:15:38 +0700

    fix: remove basePath and assetPrefix for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "last-whale"; 
 
 const nextConfig = {
   output: "export",
-
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
-
+  images: { unoptimized: true },  
   trailingSlash: true, // ✅ สำคัญมาก! ป้องกัน 404 ของ _next/static
-
-  images: {
-    unoptimized: true, // GitHub Pages ไม่รองรับ Image Optimization
-  },
-
   reactStrictMode: true,
-
   env: {
     NEXT_PUBLIC_ACCESS_PASSWORD: process.env.NEXT_PUBLIC_ACCESS_PASSWORD,
   },
